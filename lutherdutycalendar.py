417a32306fe3539002f1d65ded3a71632c3f4bcf
Hoxios <ehrhry01@luther.edu>
2017-01-10 09:19:05 -0600

    Updates as of 1/10/17

--- lutherdutycalendar.py
+++ lutherdutycalendar.py
@@ -19,20 +19,24 @@ class Conflicts:
     def make_conflicts(self):
         #makes a dictionary for each RA
         #ex.) Ryan: 1 3 5
-        RA_dict= {}
+        RA_dict= []
         self.infile.split()
+########Check to make sure this is correct!!!!!
         for item in self.infile:
+            count = -1
             if "0123456789" in item:
-                RA_dict[RA] = int(item)
+                RA_dict[count][temp].append(item)
             else:
-                RA = item
+                RA_dict.append({item: []})
+                count = count + 1
+                temp = item
 
 
 
 
 
 
-def scheduling:
+def scheduling():
     duty = Conflicts()
     duty.make_conflicts()
     #determine how many days are in the month from there assign points(I know a basic way to do this however eventually it'll need to do this on its own).
@@ -50,13 +54,29 @@ def scheduling:
     # iterate through RA_dict and schedule people one at a time
     days_remaining = []
     days_scheduled = []
-    #fills list up with days of month (temp)
+# fills list up with days of month (temp)
     for days in range(1,32):
         days_remaining.append(days)
+
+    S_RA = []
+
     while len(days_remaining) != 0:
+
         #scheduling code goes here
         #thinking of for loop of the dictionary from there getting the conflicts of the RA and then scheduling the RA
         # only if the conflicts are seperated by seven days ideally
+        if days_remaining[0] not in RA_dict[0].items(): #if the current date trying to be scheudled is not in the RA's conflict then schedule it
+            if S_RA[0][-1] < 7: #if the days are not over seven days apart
+                try:
+                    #try to schedule another RA for the position
+                    continue
+            else:
+                #Schedule the RA if that isn't an issue
+
+
+
+
+
 
 
 
